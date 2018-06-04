b020ab64d4503550fee0676adab5b5ecde78be74
Michael Bullington <mikebullingtn@gmail.com>
2018-06-03 23:46:09 -0500

    Working RTSP stream with the great potterhsu.rtsplibrary and SurfaceView.

--- android/app/src/main/java/xyz/bullington/candere/StreamActivity.kt
+++ android/app/src/main/java/xyz/bullington/candere/StreamActivity.kt
@@ -1,18 +1,38 @@
 package xyz.bullington.candere
 
-import android.net.Uri
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.Rect
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
-import android.widget.Toast
-import android.widget.VideoView
+import android.view.SurfaceView
+import com.potterhsu.rtsplibrary.NativeCallback
+import com.potterhsu.rtsplibrary.RtspClient
 
-/**
- * This activity helps us to show the live stream of an ONVIF camera thanks to VLC library.
- */
+class RtspTask(val url: String) : AsyncTask<RtspClient, Unit, Unit>() {
+    var mRtspClient: RtspClient? = null
+
+    override fun doInBackground(vararg client: RtspClient?) {
+        client.first()?.let { client ->
+            mRtspClient = client
+            mRtspClient?.play(url)
+        }
+    }
+
+    override fun onCancelled() {
+        super.onCancelled()
+
+        mRtspClient?.stop()
+        mRtspClient?.dispose()
+    }
+}
+
 class StreamActivity : AppCompatActivity() {
-
-    var videoView: VideoView? = null
+    var mSurfaceView: SurfaceView? = null
+    var mRtspClient: RtspClient? = null
+    var mRtspTask: RtspTask? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,16 +50,50 @@ class StreamActivity : AppCompatActivity() {
             }
         }
 
-        videoView = findViewById(R.id.videoView)
-        videoView?.let { videoView ->
+        mSurfaceView = findViewById(R.id.videoView)
+        mSurfaceView?.let { mSurfaceView ->
             val url = intent.getStringExtra(RTSP_URL)
-            videoView.setVideoURI(Uri.parse(url))
-            videoView.start()
+
+            mRtspClient = RtspClient(NativeCallback { frame, _, width, height ->
+                mSurfaceView.holder?.lockCanvas()?.let { canvas ->
+                    val area = width * height
+                    val pixels = IntArray(area)
+
+                    for (i in 0 until area) {
+                        var r = frame[3 * i].toInt()
+                        var g = frame[3 * i + 1].toInt()
+                        var b = frame[3 * i + 2].toInt()
+                        if (r < 0) r += 255
+                        if (g < 0) g += 255
+                        if (b < 0) b += 255
+                        pixels[i] = Color.rgb(r, g, b)
+                    }
+
+                    val bmp = Bitmap.createBitmap(pixels, width, height, Bitmap.Config.ARGB_8888)
+
+                    canvas.drawBitmap(
+                            bmp,
+                            null,
+                            Rect(0, 0, canvas.width, height * canvas.width / width),
+                            null
+                    )
+
+                    mSurfaceView.holder?.unlockCanvasAndPost(canvas)
+                }
+            })
+
+            mRtspTask = RtspTask(url)
+            mRtspTask?.execute(mRtspClient)
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mRtspTask?.cancel(true)
+    }
+
     private fun stop() {
-        videoView?.stopPlayback()
         finish()
     }
 }
